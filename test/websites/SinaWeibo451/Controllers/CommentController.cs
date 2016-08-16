61a9f64896122829e8b924041e36c6eb20b83ea6
qqbuby <qqbuby@gmail.com>
2016-08-16 09:39:38 +0800

    Add SinaWeibo451

---
+++ test/websites/SinaWeibo451/Controllers/CommentController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SinaWeibo.Models;
+
+namespace SinaWeibo.Controllers
+{
+    /// <summary>
+    /// 评论
+    /// </summary>
+    [Route("comments")]
+    public class CommentController
+    {
+        ///// <summary>
+        ///// 根据微博ID返回某条微博的评论列表
+        ///// </summary>
+        ///// <returns></returns>
+        /// <summary>
+        /// 根据微博ID返回某条微博的评论列表
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。</param>
+        /// <param name="id">需要查询的微博ID。</param>
+        /// <param name="since_id">若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。</param>
+        /// <param name="max_id">若指定此参数，则返回ID小于或等于max_id的评论，默认为0。</param>
+        /// <param name="count">单页返回的记录条数，默认为50。</param>
+        /// <param name="page">返回结果的页码，默认为1。</param>
+        /// <param name="filter_by_author">作者筛选类型，0：全部、1：我关注的人、2：陌生人，默认为0。 </param>
+        /// <returns></returns>
+        [HttpGet("show.json")]
+        public IEnumerable<Comment> Show(
+            [FromQuery] string access_token,
+            [FromQuery]long id,
+            [FromQuery] long since_id,
+            [FromQuery]long max_id,
+            [FromQuery]int count,
+            [FromQuery]int page,
+            [FromQuery]int filter_by_author)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取当前登录用户所发出的评论列表 
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。</param>
+        /// <param name="since_id">若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。</param>
+        /// <param name="max_id">若指定此参数，则返回ID小于或等于max_id的评论，默认为0。</param>
+        /// <param name="count">单页返回的记录条数，默认为50。</param>
+        /// <param name="page">返回结果的页码，默认为1。</param>
+        /// <param name="filter_by_source">来源筛选类型，0：全部、1：来自微博的评论、2：来自微群的评论，默认为0。  </param>
+        /// <returns></returns>
+        [HttpGet("by_me.json")]
+        public IList<Comment> ByMe(
+            [FromQuery] string access_token,
+            [FromQuery] long since_id,
+            [FromQuery]long max_id,
+            [FromQuery]int count,
+            [FromQuery]int page,
+            [FromQuery]int filter_by_source)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 获取当前登录用户所接收到的评论列表  
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。</param>
+        /// <param name="since_id">若指定此参数，则返回ID比since_id大的评论（即比since_id时间晚的评论），默认为0。</param>
+        /// <param name="max_id">若指定此参数，则返回ID小于或等于max_id的评论，默认为0。</param>
+        /// <param name="count">单页返回的记录条数，默认为50。</param>
+        /// <param name="page">返回结果的页码，默认为1。</param>
+        /// <param name="filter_by_author">作者筛选类型，0：全部、1：我关注的人、2：陌生人，默认为0。</param>
+        /// <param name="filter_by_source">来源筛选类型，0：全部、1：来自微博的评论、2：来自微群的评论，默认为0。  </param>
+        /// <returns></returns>
+        [HttpGet("to_me.json")]
+        public IList<Comment> ToMe(
+            [FromQuery] string access_token,
+            [FromQuery] long since_id,
+            [FromQuery]long max_id,
+            [FromQuery]int count,
+            [FromQuery]int page,
+            [FromQuery]int filter_by_author,
+            [FromQuery]int filter_by_source)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 对一条微博进行评论
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。</param>
+        /// <param name="comment">评论内容，必须做URLencode，内容不超过140个汉字。 </param>
+        /// <param name="id">需要评论的微博ID。 </param>
+        /// <param name="comment_ori">当评论转发微博时，是否评论给原微博，0：否、1：是，默认为0。 </param>
+        /// <param name="rip">开发者上报的操作用户真实IP，形如：211.156.0.1。</param>
+        /// <returns></returns>
+        [HttpPost("create.json")]
+        public Comment Create(
+            [FromQuery] string access_token,
+            [FromQuery] long comment,
+            [FromQuery]long id,
+            [FromQuery]int comment_ori,
+            [FromQuery]int rip)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 删除一条评论
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。 </param>
+        /// <param name="cid">要删除的评论ID，只能删除登录用户自己发布的评论。</param>
+        /// <returns></returns>
+        [HttpDelete("destroy.json")]
+        public Comment Destroy(string access_token, long cid)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 回复一条评论
+        /// </summary>
+        /// <param name="access_token">采用OAuth授权方式为必填参数，OAuth授权后获得。 </param>
+        /// <param name="cid">需要回复的评论ID。 </param>
+        /// <param name="id">需要评论的微博ID。 </param>
+        /// <param name="comment">回复评论内容，必须做URLencode，内容不超过140个汉字。 </param>
+        /// <param name="without_mention">回复中是否自动加入“回复@用户名”，0：是、1：否，默认为0。 </param>
+        /// <param name="comment_ori">当评论转发微博时，是否评论给原微博，0：否、1：是，默认为0。 </param>
+        /// <param name="rip">开发者上报的操作用户真实IP，形如：211.156.0.1。 </param>
+        /// <returns></returns>
+        [HttpPost("reply.json")]
+        public Comment Reply(
+                  [FromQuery] string access_token,
+                  [FromQuery] long cid,
+                  [FromQuery] long id,
+                  [FromQuery] string comment,
+                  [FromQuery] int without_mention,
+                  [FromQuery] int comment_ori,
+                  [FromQuery] string rip)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 微博.开放平台
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns></returns>
+        [HttpPost("helo.json")]
+        public Comment Helo(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/websites/SinaWeibo451/about.md
@@ -0,0 +1,73 @@
+﻿```
+```
+```
+你不是讲过，被我气都气饱了？这气饱的，果然比不上吃东西填肚子，一会儿就撑不住了。 云天河 -《仙剑奇侠传 4》
+```
+
+```text
+道贯三才为一气耳，
+
+天以气而运行，
+
+地以气而发生，
+
+阴阳以气而惨舒，
+
+风雷以气而动荡，
+
+人身以气而呼吸，
+
+道法以气而感通。
+
+水之润下，无孔不入；
+
+火之炎上，无物不焚；
+
+雷之肃敛，无坚不摧；
+
+风之肆拂，无阻不透；
+
+土之养化，无物不融!
+```
+
+```cs
+using AspNetX;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SinaWeibo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ServerOptions>(o => o.Description = "无所谓好或不好，人生一场虚空大梦，韶华白首，不过转瞬。惟有天道恒在，往复循环，不曾更改…… —— 慕容紫英.仙剑奇侠传 4》");
+            services.AddAspNetX();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseAspNetX();
+            app.UseMvc();
+        }
+    }
+}
+```
