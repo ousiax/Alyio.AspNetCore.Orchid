d8621d2cfc72ed8530f5cad0a20b7451d3e2ce2c
qqbuby <qqbuby@gmail.com>
2018-11-14 16:37:28 +0800

    Update ApiExplorerVisibilityEnabledConvention.cs

--- src/Alyio.AspNetCore.Orchid/Conventions/ApiExplorerVisibilityEnabledConvention.cs
+++ src/Alyio.AspNetCore.Orchid/Conventions/ApiExplorerVisibilityEnabledConvention.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.AspNetCore.Mvc.Routing;
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace Alyio.AspNetCore.Orchid.Conventions
 {
@@ -8,24 +6,7 @@ namespace Alyio.AspNetCore.Orchid.Conventions
     {
         public void Apply(ApplicationModel application)
         {
-            foreach (var controller in application.Controllers)
-            {
-                if (controller.ApiExplorer.IsVisible == null)
-                {
-                    if (controller.Attributes.OfType<IRouteTemplateProvider>().FirstOrDefault() != null)
-                    {
-                        controller.ApiExplorer.IsVisible = true;
-                        controller.ApiExplorer.GroupName = controller.ControllerName;
-                    }
-                }
-                else
-                {
-                    if (controller.ApiExplorer.GroupName == null)
-                    {
-                        controller.ApiExplorer.GroupName = controller.ControllerName;
-                    }
-                }
-            }
+            application.ApiExplorer.IsVisible = true;
         }
     }
 }
